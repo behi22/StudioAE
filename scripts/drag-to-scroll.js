38df9e9134dce3ba24ce6bec867789f0873cb888
Behbod Babai <behibabai@gmail.com>
2024-07-28 20:24:06 -0700

    body last section

--- scripts/drag-to-scroll.js
+++ scripts/drag-to-scroll.js
@@ -1,35 +1,34 @@
 const SCROLL_SPEED = 1;
 
 export function initDragToScroll() {
-  const spaces = document.querySelector('.spaces');
-  let isDown = false;
-  let startX;
-  let scrollLeft;
+  const spaces = document.querySelectorAll('.spaces').forEach((element) => {
+    let isDown = false;
+    let startX;
+    let scrollLeft;
 
-  if (spaces) {
-    spaces.addEventListener('mousedown', (e) => {
+    element.addEventListener('mousedown', (e) => {
       isDown = true;
-      spaces.classList.add('active');
-      startX = e.pageX - spaces.offsetLeft;
-      scrollLeft = spaces.scrollLeft;
+      element.classList.add('active');
+      startX = e.pageX - element.offsetLeft;
+      scrollLeft = element.scrollLeft;
     });
 
-    spaces.addEventListener('mouseleave', () => {
+    element.addEventListener('mouseleave', () => {
       isDown = false;
-      spaces.classList.remove('active');
+      element.classList.remove('active');
     });
 
-    spaces.addEventListener('mouseup', () => {
+    element.addEventListener('mouseup', () => {
       isDown = false;
-      spaces.classList.remove('active');
+      element.classList.remove('active');
     });
 
-    spaces.addEventListener('mousemove', (e) => {
+    element.addEventListener('mousemove', (e) => {
       if (!isDown) return;
       e.preventDefault();
-      const x = e.pageX - spaces.offsetLeft;
+      const x = e.pageX - element.offsetLeft;
       const walk = (x - startX) * SCROLL_SPEED;
-      spaces.scrollLeft = scrollLeft - walk;
+      element.scrollLeft = scrollLeft - walk;
     });
-  }
+  });
 }
